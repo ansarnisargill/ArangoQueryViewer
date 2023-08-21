1795f0d4d1dfd84aa20b159c98a597ef65ebdafd
Ansar <ansarnisargill@gmail.com>
2023-08-21 18:57:18 +0500

    updated: csharp code

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace AQLQueryRunner
+{
+    class ApplicationMain
+    {
+        static void Main(string[] args)
+        {
+            QueryRunner<Root>.QueryGraph("mps_verts", "mps_graph", 3, new string[] { "A", "B" }).Wait();
+        }
+    }
+
+    public class Neighbor
+    {
+        public string _id { get; set; }
+        public string _key { get; set; }
+        public string _rev { get; set; }
+        public List<Neighbor> neighbors { get; set; }
+    }
+
+    public class Root
+    {
+        public Neighbor Result { get; set; }
+    }
+}
---
+++ QueryRunner.cs
@@ -0,0 +1,104 @@
+﻿using ArangoDBNetStandard.Transport.Http;
+using ArangoDBNetStandard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AQLQueryRunner
+{
+    public class QueryRunner<T>
+    {
+        public const string DB_URL = @"http://localhost:8529/";
+        public const string NEIGHBOR_NAME_PREFIX = "neighboursLevel";
+
+        public static async Task<List<T>> QueryGraph(string verticesCollection,
+            string graphCollection,
+            int level,
+            string[] allowedKeys)
+        {
+            var levelQuery = ConstructNeighbourLevelQuery(level, graphCollection);
+            var query = $"""
+                    FOR item IN {verticesCollection}
+                    FILTER item._key IN [{FormatAllowedKeys(allowedKeys)}]
+                    LET neighborId0 = item._id
+                    {levelQuery}
+                    LET itemWithNeighbors = MERGE(item, {GetResultShape("neighboursLevel1")})
+                    RETURN {GetResultShape("itemWithNeighbors", "Result")}
+                    """;
+
+
+            using var systemDbTransport = HttpApiTransport.UsingNoAuth(new Uri(DB_URL));
+            var adb = new ArangoDBClient(systemDbTransport);
+            var response = await adb.Cursor.PostCursorAsync<T>(query);
+            var result = response.Result.ToList();
+            return result;
+        }
+        private static string GetResultShape(string resultProperty, string propertyName = "neighbors")
+        {
+            return "{ " + propertyName + ":  " + resultProperty + "}";
+        }
+        private static string FormatAllowedKeys(string[] allowedKeys)
+        {
+            var val = "";
+
+            foreach (var item in allowedKeys)
+            {
+                val += $"\"{item}\",";
+            }
+            return val.Substring(0, val.Length - 1);
+        }
+
+        private static string ConstructNeighbourLevelQuery(int level, string graphName)
+        {
+            var queries = new List<string>();
+            var innerQueryOptions = """{ bfs: true, uniqueEdges: "path" }""";
+
+            for (var i = level; i > 0; i--)
+            {
+                var filter = $"""
+                 v{i}.type != "type" OR v{i}.field != ""
+                 """;
+
+                if (i == level)
+                {
+                    queries.Add($"""
+                  LET {NEIGHBOR_NAME_PREFIX + i} = (
+                            FOR v{i} IN 1..1 ANY neighborId{i - 1} GRAPH "{graphName}"
+                            OPTIONS {innerQueryOptions}
+                            FILTER {filter}
+                            LET neighborId{i} = v{i}._id
+                                PASTE_TEMPLATE
+                            RETURN v{i}
+                        )
+                """);
+                }
+                else
+                {
+                    queries.Add($"""
+                  LET {NEIGHBOR_NAME_PREFIX + i} = (
+                            FOR v{i} IN 1..1 ANY neighborId{i - 1} GRAPH "{graphName}"
+                            OPTIONS {innerQueryOptions}
+                            FILTER {filter}
+                            LET neighborId{i} = v{i}._id
+                                PASTE_TEMPLATE
+                            RETURN MERGE(v{i}, {GetResultShape(NEIGHBOR_NAME_PREFIX + (i + 1))})
+                        )
+                """);
+                }
+
+
+            }
+
+
+            var compiledQuery = "";
+            queries.ForEach(q =>
+            {
+                q = q.Replace("PASTE_TEMPLATE", compiledQuery);
+                compiledQuery = q;
+            });
+            return compiledQuery;
+        }
+    }
+}
